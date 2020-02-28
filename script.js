61058360f076a0d2162a956ae3ea5c33dddb5d18
Turan98 <48137040+Turan98@users.noreply.github.com>
2020-02-28 17:54:40 +0400

    sinaaaaan

---
+++ script.js
@@ -0,0 +1,16 @@
+/*function myFunction(){
+    var x= document.getElementsByClassName("city")
+    for(var i=0 ; i<x.length; i++) {
+        x[i].style.display = "none";
+    }
+}
+function displayResult(){
+    
+    document.getElementById("header").innerHTML = "Hello Turan";}*/
+/*function myFunction(){
+    document.getElementById("demo").innerHTML = "Hello Turan";
+}*/
+function myFunction(){
+    document.getElementById("demo").style.fontSize = "50px";
+   
+}
